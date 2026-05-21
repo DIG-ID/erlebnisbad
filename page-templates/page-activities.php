cc89cccda2effc0f26d3eccb5d03636c402bbb5f
Pedro <pedro@dig.id>
2026-05-21 19:05:39 +0100

    up

--- page-templates/page-activities.php
+++ page-templates/page-activities.php
@@ -14,11 +14,12 @@ if ( have_posts() ) :
 		the_post();
 		do_action( 'before_main_content' );
 			get_template_part( 'template-parts/modules/hero' );
-      get_template_part( 'template-parts/pages/activities/intro' );
-      get_template_part( 'template-parts/pages/activities/content-kursangebot' );
-      get_template_part( 'template-parts/pages/activities/content-angebot' );
-      get_template_part( 'template-parts/pages/activities/content-besonderes-erlebnis' );
-      get_template_part( 'template-parts/pages/activities/opening-hours' );
+			get_template_part( 'template-parts/pages/activities/intro' );
+			get_template_part( 'template-parts/pages/activities/content-kursangebot' );
+			get_template_part( 'template-parts/pages/activities/content-angebot' );
+			get_template_part( 'template-parts/pages/activities/content-besonderes-erlebnis' );
+			do_action( 'wave_separator' );
+			get_template_part( 'template-parts/pages/activities/opening-hours' );
 		do_action( 'after_main_content' );
 	endwhile;
 endif;
