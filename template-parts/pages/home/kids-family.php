ae197414868c3c95e8e9d7b8a34bc720f33ff2ad
Pedro <pedro@dig.id>
2026-05-22 12:26:57 +0100

    up

--- template-parts/pages/home/kids-family.php
+++ template-parts/pages/home/kids-family.php
@@ -16,7 +16,7 @@
         $img_id = get_field( 'kids_family_image' );
         if ( $img_id ) :
           ?>
-          <div class="shape-bg shape-bg__img shape-bg--1 before:bg-Mint2">
+          <div class="shape-bg shape-bg__img shape-bg--1 before:bg-Mint3">
             <?php echo wp_get_attachment_image( $img_id, 'full' ); ?>
           </div>
           <?php
