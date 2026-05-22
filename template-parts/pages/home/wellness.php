14fe5868f444b9e8278f1b37114e00202d86c651
Pedro <pedro@dig.id>
2026-05-22 10:13:17 +0100

    up

--- template-parts/pages/home/wellness.php
+++ template-parts/pages/home/wellness.php
@@ -8,44 +8,47 @@
  */
 
 ?>
+
 <section id="section-wellness" class="section-wellness bg-Mint1">
-  <div class="theme-container pt-36 md:pt-24 xl:pt-10 pb-32 md:pb-24 xl:pb-36">
-    <div class="theme-grid items-center">
-      <div class="col-span-2 md:col-span-3 xl:col-start-2 xl:col-span-5 order-2 md:order-none">
-        <?php
-        $img_id = get_field( 'wellness_image' );
-        if ( $img_id ) :
-          ?>
-          <div class="image-fill image-fill--1 image-fill--big image-fill--mint-2">
-            <?php
-            echo wp_get_attachment_image(
-              $img_id,
-              'full',
-              false,
-              array(
-                'class' => 'object-cover',
-              )
-            );
-            ?>
-          </div>
-          <?php
-        endif;
-        ?>
-      </div>
-      <div class="col-span-2 md:col-start-4 md:col-span-3 xl:col-start-7 xl:col-span-5">
-        <p class="overtitle text-Black pb-8 xl:pb-4"><?php the_field( 'wellness_overtitle' ); ?></p>
-        <h2 class="title-main text-Black pb-8"><?php the_field( 'wellness_title' ); ?></h2>
-        <p class="text-Black pb-8"><?php the_field( 'wellness_text' ); ?></p>
-        <?php
-        $hero_button = get_field( 'wellness_button' );
-        if ( $hero_button ) :
-            $link_url    = $hero_button['url'];
-            $link_title  = $hero_button['title'];
-            $link_target = $hero_button['target'] ? $hero_button['target'] : '_self';
-            ?>
-            <a class="btn btn-primary" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-        <?php endif; ?>
-      </div>
-    </div>
-  </div>
+
+	<div class="theme-container pt-36 md:pt-24 xl:pt-10 pb-32 md:pb-24 xl:pb-36">
+		<div class="theme-grid items-center">
+			<div class="col-span-2 md:col-span-3 xl:col-start-2 xl:col-span-5 order-2 md:order-none">
+				<?php
+				$img_id = get_field( 'wellness_image' );
+				if ( $img_id ) :
+					?>
+					<div class="image-fill image-fill--1 image-fill--big image-fill--mint-2">
+						<?php
+						echo wp_get_attachment_image(
+							$img_id,
+							'full',
+							false,
+							array(
+								'class' => 'object-cover',
+							)
+						);
+						?>
+					</div>
+					<?php
+				endif;
+				?>
+			</div>
+			<div class="col-span-2 md:col-start-4 md:col-span-3 xl:col-start-7 xl:col-span-5">
+				<p class="overtitle text-Black pb-8 xl:pb-4"><?php the_field( 'wellness_overtitle' ); ?></p>
+				<h2 class="title-main text-Black pb-8"><?php the_field( 'wellness_title' ); ?></h2>
+				<p class="text-Black pb-8"><?php the_field( 'wellness_text' ); ?></p>
+				<?php
+				$hero_button = get_field( 'wellness_button' );
+				if ( $hero_button ) :
+						$link_url    = $hero_button['url'];
+						$link_title  = $hero_button['title'];
+						$link_target = $hero_button['target'] ? $hero_button['target'] : '_self';
+						?>
+						<a class="btn btn-primary" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+
 </section>
