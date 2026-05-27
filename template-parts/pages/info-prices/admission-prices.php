d9f660bdf542818070a48c3b88c9a71151370f29
bankz91 <brunoduarte.91300@gmail.com>
2026-05-27 17:08:55 +0100

    1

--- template-parts/pages/info-prices/admission-prices.php
+++ template-parts/pages/info-prices/admission-prices.php
@@ -11,7 +11,7 @@
 <section id="section-admission-prices" class="section-admission-prices bg-Mint1 waves waves__top--color pt-24 md:pt-36 xl:pt-44 pb-12 md:pb-28 xl:pb-48">
     <div class="theme-container">
         <div class="theme-grid grid-flow-row-dense">
-            <div class="col-span-2 md:col-start-4 md:col-span-3 xl:col-start-7 xl:col-span-4">
+            <div class="col-span-2 md:col-start-4 md:col-span-3 xl:col-start-8 xl:col-span-4">
                 <?php $img_id = get_field( 'prices_info_admission_prices_image' ); if ( $img_id ) : ?>
                 <figure class="shape-bg shape-bg__img shape-bg--1 before:bg-Mint2">
                     <?php echo wp_get_attachment_image( $img_id, 'full' ); ?>
@@ -21,7 +21,7 @@
             <div class="col-span-2 md:col-start-1 md:col-span-3 xl:col-start-2 xl:col-span-5 xl:pt-40 text-center justify-items-center md:text-left md:justify-items-start">
                 <p class="overtitle text-Black pb-2 md:pb-3 xl:pb-4"><?php the_field( 'prices_info_admission_prices_overtitle' ); ?></p>
                 <h2 class="title-main text-Black pb-8"><?php the_field( 'prices_info_admission_prices_title' ); ?></h2>
-                <p class="text-Black pb-8"><?php the_field( 'prices_info_admission_prices_text' ); ?></p>
+                <p class="text-Black pb-8 xl:max-w-xl"><?php the_field( 'prices_info_admission_prices_text' ); ?></p>
                 <?php
                 $hero_button = get_field( 'prices_info_admission_prices_button' );
                 if ( $hero_button ) :
