d76ba7868645fc5dd037a49769552d9ab2cb5b5c
bankz91 <brunoduarte.91300@gmail.com>
2026-05-27 10:59:50 +0100

    1

--- template-parts/pages/home/wellness.php
+++ template-parts/pages/home/wellness.php
@@ -9,23 +9,10 @@
 
 ?>
 
-<section id="section-wellness" class="section-wellness bg-Mint1 pt-36 md:pt-24 xl:pt-10 pb-32 md:pb-24 xl:pb-56">
-
-	<div class="theme-container ">
+<section id="section-wellness" class="section-wellness bg-Mint3">
+	<div class="theme-container pt-36 md:pt-24 xl:pt-10 pb-32 md:pb-24 xl:pb-36">
 		<div class="theme-grid items-center">
-			<div class="col-span-2 md:col-span-3 xl:col-start-2 xl:col-span-4 order-2 md:order-none">
-				<?php
-				$img_id = get_field( 'wellness_image' );
-				if ( $img_id ) :
-					?>
-					<figure class="shape-bg shape-bg__img shape-bg--1 before:bg-Mint2">
-						<?php echo wp_get_attachment_image( $img_id, 'full' ); ?>
-					</figure>
-					<?php
-				endif;
-				?>
-			</div>
-			<div class="col-span-2 md:col-start-4 md:col-span-3 xl:col-start-7 xl:col-span-5">
+			<div class="col-span-2 md:col-span-3 xl:col-start-2 xl:col-span-5">
 				<p class="overtitle text-Black pb-8 xl:pb-4"><?php the_field( 'wellness_overtitle' ); ?></p>
 				<h2 class="title-main text-Black pb-8"><?php the_field( 'wellness_title' ); ?></h2>
 				<p class="text-Black pb-8"><?php the_field( 'wellness_text' ); ?></p>
@@ -39,7 +26,18 @@
 						<a class="btn btn-primary" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
 				<?php endif; ?>
 			</div>
+			<div class="col-span-2 md:col-start-4 md:col-span-3 xl:col-start-8 xl:col-span-4 xl:pt-8">
+				<?php
+				$img_id = get_field( 'wellness_image' );
+				if ( $img_id ) :
+					?>
+					<figure class="shape-bg shape-bg__img shape-bg--2 before:bg-Mint1">
+						<?php echo wp_get_attachment_image( $img_id, 'full' ); ?>
+					</figure>
+					<?php
+				endif;
+				?>
+			</div>
 		</div>
 	</div>
-
 </section>
