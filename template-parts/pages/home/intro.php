d0b07a80619190a557499008aa3c3eed0180a0f5
Daniel-Digid <daniel@dig.id>
2026-06-10 13:53:45 +0100

    d

--- template-parts/pages/home/intro.php
+++ template-parts/pages/home/intro.php
@@ -19,62 +19,56 @@
             <p class="text-Black"><?php the_field( 'intro_text' ); ?></p>
         </div>
     </div>
+  </div>
 
-    <?php if ( get_field( 'intro_enable_info_block' ) ) : ?>
-    <div class="theme-grid">
-      <div class="col-span-2 md:col-span-6 xl:col-span-12 -mx-8 md:-mx-9 xl:-mx-12 pt-40">
+  <?php if ( get_field( 'intro_enable_info_block' ) ) : ?>
+  <div class="pt-40">
+    <div class="intro-slider bg-Mint relative">
+      <div class="intro-slider__inner relative px-8 md:px-14 xl:px-20 mx-auto xl:max-w-[1920px] py-64 md:pt-48 md:pb-64 xl:pt-64 xl:pb-56">
 
-        <div class="intro-slider bg-Mint relative">
-          <div class="intro-slider__inner px-8 md:px-14 xl:px-20 mx-auto xl:max-w-[1920px] py-64 md:pt-48 md:pb-64 xl:pt-64 xl:pb-56">
-
-            <?php if ( have_rows( 'intro_info_block' ) ) : ?>
-            <div class="intro-slider__swiper swiper">
-              <div class="swiper-wrapper">
-                <?php while ( have_rows( 'intro_info_block' ) ) : the_row(); ?>
-                <div class="swiper-slide">
-                  <div class="grid grid-cols-2 md:grid-cols-6 xl:grid-cols-12 gap-x-5 md:gap-x-4 xl:gap-x-5 items-start">
-                    <div class="col-span-2 md:col-span-3 xl:col-start-2 xl:col-span-4 text-left">
-                      <h3 class="title-main text-white"><?php the_sub_field( 'title' ); ?></h3>
-                    </div>
-                    <div class="col-span-2 md:col-span-3 xl:col-start-7 xl:col-span-5 text-left mt-8 md:mt-0">
-                      <p class="text-white mb-8"><?php the_sub_field( 'text' ); ?></p>
-                      <?php
-                      $link = get_sub_field( 'button' );
-                      if ( $link ) :
-                          $link_url    = $link['url'];
-                          $link_title  = $link['title'];
-                          $link_target = $link['target'] ?: '_self';
-                      ?>
-                      <a class="btn btn-primary" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-                      <?php endif; ?>
-                    </div>
-                  </div>
+        <?php if ( have_rows( 'intro_info_block' ) ) : ?>
+        <div class="intro-slider__swiper swiper">
+          <div class="swiper-wrapper items-center">
+            <?php while ( have_rows( 'intro_info_block' ) ) : the_row(); ?>
+            <div class="swiper-slide">
+              <div class="grid grid-cols-2 md:grid-cols-6 xl:grid-cols-12 gap-x-5 md:gap-x-4 xl:gap-x-5 items-start">
+                <div class="col-span-2 md:col-span-3 xl:col-start-2 xl:col-span-4 text-left">
+                  <h3 class="title-main text-white"><?php the_sub_field( 'title' ); ?></h3>
                 </div>
-                <?php endwhile; ?>
+                <div class="col-span-2 md:col-span-3 xl:col-start-7 xl:col-span-5 text-left mt-8 md:mt-0">
+                  <p class="text-white mb-8"><?php the_sub_field( 'text' ); ?></p>
+                  <?php
+                  $link = get_sub_field( 'button' );
+                  if ( $link ) :
+                      $link_url    = $link['url'];
+                      $link_title  = $link['title'];
+                      $link_target = $link['target'] ?: '_self';
+                  ?>
+                  <a class="btn btn-primary" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                  <?php endif; ?>
+                </div>
               </div>
             </div>
-
-            
-            <?php endif; ?>
-
+            <?php endwhile; ?>
           </div>
-          <div class="intro-slider__pagination">
-              <button class="intro-slider__prev" aria-label="<?php esc_attr_e( 'Previous', 'erlebnisbad' ); ?>">
-                <svg xmlns="http://www.w3.org/2000/svg" width="31" height="17" viewBox="0 0 31 17" fill="none" class="scale-x-[-1]" aria-hidden="true">
-                  <path d="M13.6394 4.95379C17.7041 3.57604 15.0846 0.418682 17.6137 0.0168392C19.016 -0.205953 21.9956 1.82167 25.1956 3.74824C27.7675 5.29659 30.666 6.86343 30.9761 8.39817C31.318 10.0898 27.9222 11.551 24.8343 13.4499C21.6348 15.4175 18.8083 17.4962 17.6134 16.8943C15.2652 15.7115 17.6137 12.5888 12.8264 11.8425C4.697 10.8092 -5.16121e-07 11.0963 -7.51996e-07 8.39817C-9.87871e-07 5.70007 9.57465 6.33154 13.6394 4.95379Z" fill="currentColor"/>
-                </svg>
-              </button>
-              <button class="intro-slider__next" aria-label="<?php esc_attr_e( 'Next', 'erlebnisbad' ); ?>">
-                <svg xmlns="http://www.w3.org/2000/svg" width="31" height="17" viewBox="0 0 31 17" fill="none" aria-hidden="true">
-                  <path d="M13.6394 4.95379C17.7041 3.57604 15.0846 0.418682 17.6137 0.0168392C19.016 -0.205953 21.9956 1.82167 25.1956 3.74824C27.7675 5.29659 30.666 6.86343 30.9761 8.39817C31.318 10.0898 27.9222 11.551 24.8343 13.4499C21.6348 15.4175 18.8083 17.4962 17.6134 16.8943C15.2652 15.7115 17.6137 12.5888 12.8264 11.8425C4.697 10.8092 -5.16121e-07 11.0963 -7.51996e-07 8.39817C-9.87871e-07 5.70007 9.57465 6.33154 13.6394 4.95379Z" fill="currentColor"/>
-                </svg>
-              </button>
-            </div>
+        </div>
+        <?php endif; ?>
+
+        <div class="intro-slider__pagination">
+          <button class="intro-slider__prev" aria-label="<?php esc_attr_e( 'Previous', 'erlebnisbad' ); ?>">
+            <svg xmlns="http://www.w3.org/2000/svg" width="31" height="17" viewBox="0 0 31 17" fill="none" class="scale-x-[-1]" aria-hidden="true">
+              <path d="M13.6394 4.95379C17.7041 3.57604 15.0846 0.418682 17.6137 0.0168392C19.016 -0.205953 21.9956 1.82167 25.1956 3.74824C27.7675 5.29659 30.666 6.86343 30.9761 8.39817C31.318 10.0898 27.9222 11.551 24.8343 13.4499C21.6348 15.4175 18.8083 17.4962 17.6134 16.8943C15.2652 15.7115 17.6137 12.5888 12.8264 11.8425C4.697 10.8092 -5.16121e-07 11.0963 -7.51996e-07 8.39817C-9.87871e-07 5.70007 9.57465 6.33154 13.6394 4.95379Z" fill="currentColor"/>
+            </svg>
+          </button>
+          <button class="intro-slider__next" aria-label="<?php esc_attr_e( 'Next', 'erlebnisbad' ); ?>">
+            <svg xmlns="http://www.w3.org/2000/svg" width="31" height="17" viewBox="0 0 31 17" fill="none" aria-hidden="true">
+              <path d="M13.6394 4.95379C17.7041 3.57604 15.0846 0.418682 17.6137 0.0168392C19.016 -0.205953 21.9956 1.82167 25.1956 3.74824C27.7675 5.29659 30.666 6.86343 30.9761 8.39817C31.318 10.0898 27.9222 11.551 24.8343 13.4499C21.6348 15.4175 18.8083 17.4962 17.6134 16.8943C15.2652 15.7115 17.6137 12.5888 12.8264 11.8425C4.697 10.8092 -5.16121e-07 11.0963 -7.51996e-07 8.39817C-9.87871e-07 5.70007 9.57465 6.33154 13.6394 4.95379Z" fill="currentColor"/>
+            </svg>
+          </button>
         </div>
 
       </div>
     </div>
-    <?php endif; ?>
-
   </div>
+  <?php endif; ?>
 </section>
